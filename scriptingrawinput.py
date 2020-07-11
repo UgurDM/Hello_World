118155112db4b9c4606525879afdb7e3aedcdb4c
UgurDM <ugur.demirelce@gmail.com>
2020-07-11 17:10:41 +0300

    Ugur new works

---
+++ scriptingrawinput.py
@@ -0,0 +1,19 @@
+names =  [item for item in input("Enter Student Names : ").split(",")]  # get and process input for a list of names
+assignments = [int(item) for item in input("Enter Assigment Counts : ").split(",")]  # get and process input for a list of the number of assignments
+grades =  [int(item) for item in input("Enter Grades : ").split(",")] # get and process input for a list of grades
+
+#a = list(zip(names, assignments, grades))
+#
+#print(a)
+#print(type(a))
+
+# message string to be used for each student
+# HINT: use .format() with this string in your for loop
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+# write a for loop that iterates through each set of names, assignments, and grades to print each student's message
+for name, assigment, grade in zip(names, assignments, grades):
+    max_grade = grade + 2*assigment
+    print(message.format(name.title(), assigment, grade, max_grade))
